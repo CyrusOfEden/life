c6e3cbfa596198f31485b996db93e09e8ebcb5e4
Kash Nouroozi <knouroozi@gmail.com>
2015-08-17 19:46:59 -0400

    Refactor! Performance! Life!

--- lib/life.ex
+++ lib/life.ex
@@ -1,17 +1,28 @@
 defmodule Life do
-  def play({nr, nc}, opts \\ []) do
-    opts = Keyword.merge([generations: 24, seed: []], opts)
+  @period 200 # milliseconds per tick
+  @defaults [
+    board: {14, 80},
+    generations: 24,
+    seed: [
+      {2, 3}, {2, 4}, {2, 5}, {3, 4},
+      {11, 24}, {12, 25}, {12, 26}, {13, 26}
+    ]
+  ]
 
-    range = 1..opts[:generations]
-    start = tick(board(nr, nc), opts[:seed])
+  def play(opts \\ []) do
+    opts = Keyword.merge(@defaults, opts)
+
+    generations = 1..opts[:generations]
+    {rows, columns} = opts[:board]
+    initial = seed(board(rows, columns), opts[:seed])
     next = fn _, board ->
-      future = tick(board, next_state(board))
+      future = tick(board)
+      future |> inspect_board |> display_board
       {future, future}
     end
 
-    {ticks,_} = Enum.map_reduce(range, start, next)
-
-    Enum.each(ticks, &show/1)
+    Enum.map_reduce(generations, initial, next)
+    :ok
   end
 
   def board(nr, nc) do
@@ -22,61 +33,68 @@ defmodule Life do
     end
   end
 
-  def get([row|_] = board, {r, c}) when r < length(board) and
-                                        c < length(row) do
-    board |> Enum.at(r) |> Enum.at(c)
+  def seed(board, [{_,_,}|_] = seeds) do
+    seeds
+    |> Enum.group_by(&elem(&1, 0))
+    |> Enum.map(fn {r, coords} ->
+      {r, Enum.map(coords, &elem(&1, 1))}
+    end)
+    |> Enum.reduce(board, fn {r, cs}, board ->
+      List.update_at board, r, fn row ->
+        Enum.reduce cs, row, fn c, row ->
+          List.replace_at row, c, true
+        end
+      end
+    end)
   end
-  def get(_, _), do: nil
 
-  def update(board, {cr, cc, value}) do
-    board |> List.update_at cr, fn row ->
-      row |> List.replace_at cc, value
+  def get([row|_] = board, {r, c}) do
+    case {r < length(board), c < length(row)} do
+      {true, true} -> board |> Enum.at(r) |> Enum.at(c)
+      _            -> nil
     end
   end
 
-  @neighbours [
+  @neighbour_coords [
     {-1, -1}, {-1, 0}, {-1, 1},
     { 0, -1},          { 0, 1},
     { 1, -1}, { 1, 0}, { 1, 1}
   ]
   def get_neighbours(board, {r, c}) do
-    Enum.count @neighbours, fn {nr, nc} ->
+    Enum.count @neighbour_coords, fn {nr, nc} ->
       get(board, {r + nr, c + nc})
     end
   end
 
-  def tick(board, []), do: board
-  def tick(board, [{_,_,_}|_] = coords) do
-    Enum.reduce coords, board, &update(&2, &1)
-  end
-
-  def next_state(board) do
-    board |> Enum.with_index |> Enum.flat_map fn {row, r} ->
+  def tick(board) do
+    board |> Enum.with_index |> Enum.map fn {row, r} ->
       row |> Enum.with_index |> Enum.map fn {cell, c} ->
-        {r, c, next_cell_state(board, cell, {r, c})}
+        next_cell_state(board, cell, {r, c})
       end
     end
   end
 
   def next_cell_state(board, cell, coord) do
     case {cell, get_neighbours(board, coord)} do
-      {true,  n} when n <= 3 -> true
-      {true,  n} when n > 3  -> false
-      {false, n} when n == 3 -> true
-      _                      -> false
+      {true, n} when n < 3  -> true
+      {true, n} when n > 3  -> false
+      {_,    n} when n == 3 -> true
+      _                     -> false
     end
   end
 
-  def show(board) do
-    Enum.each board, fn row ->
-      Enum.each row, fn cell ->
-        IO.write show_cell(cell)
-      end
-      IO.puts ""
+  def inspect_cell(cell) when cell == true,  do: "◉"
+  def inspect_cell(cell) when cell == false, do: " "
+
+  def inspect_board(board) do
+    Enum.map_join board, "\n", fn row ->
+      Enum.map_join row, &inspect_cell/1
     end
-    :timer.sleep(100)
   end
 
-  def show_cell(cell) when cell == true, do: "x"
-  def show_cell(cell) when cell == false, do: "."
+  def display_board(representation) when is_binary(representation) do
+    IO.write :os.cmd('clear')
+    IO.puts representation
+    :timer.sleep(@period)
+  end
 end
